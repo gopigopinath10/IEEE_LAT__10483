f29eb732f199ea6adfc685b874c220adad81cb85
gopigopinath10 <gopigopinath10@gmail.com>
2026-04-10 14:21:12 +0530

    Add files via upload

---
+++ Fext_01.m
@@ -0,0 +1,30 @@
+% Read CSV file
+data = readtable('Fig.8.csv');
+
+% Extract columns
+freq = data{:,1};   % Frequency (GHz)
+
+y1 = data{:,2};
+y2 = data{:,3};
+y3 = data{:,4};
+y4 = data{:,5};
+y5 = data{:,6};
+
+% Plot
+figure;
+plot(freq, y1, 'r', 'LineWidth', 2); hold on;
+plot(freq, y2, 'b', 'LineWidth', 2);
+plot(freq, y3, 'g', 'LineWidth', 2);
+plot(freq, y4, 'm', 'LineWidth', 2);
+plot(freq, y5, 'k', 'LineWidth', 2);
+
+% Labels and title
+xlabel('Frequency (GHz)');
+ylabel('Magnitude (dB)');
+title('S-Parameter Plot');
+
+% Legend
+legend('d=0.2mm','d=0.4mm','d=0.6mm','d=0.8mm','d=1 mm','Location','best');
+
+% Grid
+grid on;
---
+++ Next_comp.m
@@ -0,0 +1,28 @@
+% Read CSV file
+data = readtable('Fig.15.csv');
+
+% Extract columns
+freq = data{:,1};   % Frequency (GHz)
+
+y1 = data{:,2};
+y2 = data{:,3};
+y3 = data{:,4};
+
+
+% Plot
+figure;
+plot(freq, y1, 'r', 'LineWidth', 2); hold on;
+plot(freq, y2, 'b', 'LineWidth', 2);
+plot(freq, y3, 'g', 'LineWidth', 2);
+
+
+% Labels and title
+xlabel('Frequency (GHz)');
+ylabel('Magnitude (dB)');
+title('S-Parameter Plot');
+
+% Legend
+legend('No Gurard','With Guard','Proposed','Location','best');
+
+% Grid
+grid on;
